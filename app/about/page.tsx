d362854c628ad51dd366c299b077dd57f435a47d
Areej Mirani <am1r4n1@gmail.com>
2025-01-08 07:10:41 -0500

    fix about page header

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,22 +1,52 @@
 import Image from 'next/image';
+import { useState } from 'react';
 import NavLinks from "../components/nav-links";
 
 export default function about() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const handleToggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+  
+
   return (
     <div className="min-h-screen bg-light-blue text-dark-blue font-sans flex flex-col">
-      <header className="bg-dark-blue text-white p-6">
       {/* Header Section */}
-            <nav className="flex justify-between items-center">
-            {/* <h1 className="text-4xl font-bold">Project Skyward</h1> */}
-          <Image
-            src="/logo.png"
-            alt="Project Skyward Logo"
-            width={100}
-            height={100}
-            className="mr-4"
-          />
-            <NavLinks />
-            </nav>
+      <header className="bg-dark-blue text-white p-6">
+        <nav className="flex justify-between items-center">
+          {/* Logo */}
+          <div>
+            <Image
+              src="/logo.png"
+              alt="Project Skyward Logo"
+              width={150}
+              height={150}
+              className="mr-4"
+            />
+          </div>
+
+
+          {/* NavLinks */}
+          <NavLinks isNavOpen={isNavOpen} />
+
+
+          {/* Mobile Menu Button */}
+          <div className="flex items-center gap-6">
+            <button
+              className="bg-blue text-white px-6 py-3 rounded-lg hover:bg-dark-blue hover:text-white"
+              onClick={() => window.location.href = "/donate"}
+            >
+              Donate
+            </button>
+            <button
+              className="text-3xl md:hidden text-white"
+              onClick={handleToggleNav}
+              aria-label="Toggle navigation menu"
+            >
+              {isNavOpen ? "✖" : "☰"}
+            </button>
+          </div>
+        </nav>
       </header>
 
       <main className="flex-grow max-w-4xl mx-auto p-8 space-y-8">
